98d4e6547e9c8ae2a87fa08c1385666851ed2baa
iftikhrlutfur <iftikherlutfur@gmail.com>
2024-05-14 03:09:27 +0600

    added the my list routes where user can only see their booked list

--- src/Components/MyList/MyList.jsx
+++ src/Components/MyList/MyList.jsx
@@ -1,85 +1,85 @@
-// import Table from '@mui/material/Table';
-// import TableBody from '@mui/material/TableBody';
-// import TableCell from '@mui/material/TableCell';
-// import TableContainer from '@mui/material/TableContainer';
-// import TableHead from '@mui/material/TableHead';
-// import TableRow from '@mui/material/TableRow';
-// import Paper from '@mui/material/Paper';
-// import { useLoaderData } from 'react-router-dom';
-// import { useContext } from 'react';
-// import { AuthContext } from '../../Authentication/AuthProvider';
-// import { FaEdit } from "react-icons/fa";
-// import { MdDeleteForever } from "react-icons/md";
-// import toast, { Toaster } from 'react-hot-toast';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { useLoaderData } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../Authentication/AuthProvider';
+import { FaEdit } from "react-icons/fa";
+import { MdDeleteForever } from "react-icons/md";
+import toast, { Toaster } from 'react-hot-toast';
 
 
 
 
-// const MyList = () => {
+const MyList = () => {
 
 
-//     const {user} = useContext(AuthContext)
-// const handleDelete = id =>{
-//   console.log(id);
-//   fetch(`http://localhost:5000/post/${id}`,{
-//     method:"DELETE"
-//   })
-//   .then(res=>res.json())
-//   .then(data=>{console.log(data)
-//     if(data.deletedCount>0){
-//       toast.success('Successfully Deleted!')
+    const {user} = useContext(AuthContext)
+const handleDelete = id =>{
+  console.log(id);
+  fetch(`http://localhost:5000/post/${id}`,{
+    method:"DELETE"
+  })
+  .then(res=>res.json())
+  .then(data=>{console.log(data)
+    if(data.deletedCount>0){
+      toast.success('Successfully Deleted!')
   
-//     }
+    }
 
-//   })
-// }
+  })
+}
 
-// const data = useLoaderData()
-// console.log(data);
+const data = useLoaderData()
+console.log(data);
 
-// const data2 = data.filter(dt=>dt.email === user.email)
-// console.log(data2);
+const data2 = data.filter(dt=>dt.email === user.email)
+console.log(data2);
 
 
-//     return (
+    return (
 
-//              <TableContainer component={Paper}>
-//       <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-//         <TableHead>
-//           <TableRow>
-//             <TableCell>Room</TableCell>
-//             <TableCell align="right">Price</TableCell>
-//             <TableCell align="right">Date</TableCell>
-//             <TableCell align="right">Update</TableCell>
-//             <TableCell align="right">Delete</TableCell>
-//           </TableRow>
-//         </TableHead>
-//         <TableBody>
-//           {data2.map((row) => (
-//             <TableRow
-//               key={row._id}
-//               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-//             >
-//               <TableCell component="th" scope="row">
-//                 {row.item}
-//               </TableCell>
-//               <TableCell align="right">{'$' + row.price_per_night}</TableCell>
-//               <TableCell align="right">{row.date}</TableCell>
-//               <TableCell align="right"><button><FaEdit></FaEdit></button></TableCell>
-//               <TableCell align="right">
-//               <div>
-//               <button onClick={()=>handleDelete(row._id)}><MdDeleteForever></MdDeleteForever></button>
-//               </div></TableCell>
+             <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Room</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">Date</TableCell>
+            <TableCell align="right">Update</TableCell>
+            <TableCell align="right">Delete</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data2.map((row) => (
+            <TableRow
+              key={row._id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.item}
+              </TableCell>
+              <TableCell align="right">{'$' + row.price_per_night}</TableCell>
+              <TableCell align="right">{row.date}</TableCell>
+              <TableCell align="right"><button><FaEdit></FaEdit></button></TableCell>
+              <TableCell align="right">
+              <div>
+              <button onClick={()=>handleDelete(row._id)}><MdDeleteForever></MdDeleteForever></button>
+              </div></TableCell>
               
-//               <Toaster />
+              <Toaster />
        
-//             </TableRow>
+            </TableRow>
              
-//           ))}
-//         </TableBody>
-//       </Table>
-//     </TableContainer>
-//   );
-// }
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
 
-// export default MyList;
+export default MyList;
